f8c679894748e162076d01432225d202c0896236
Leandro <leandro_santos16@hotmail.com>
2019-06-04 20:05:51 -0300

    Exercicio 3 e 4

---
+++ CarteiraDeFundosMobile/FundsWallet/FundsWallet/Model/Fund.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundsWallet.Model
+{
+    public class Fundo
+    {
+        public int FundoId { get; set; }
+        public string Nome { get; set; }
+        public string Valor { get; set; }
+        public string Qtd { get; set; }
+
+    }
+}
